65709e81f735b58954bfbcb76f3edc3cf1809151
Evan Dixon <evanldixon@live.com>
2017-02-16 12:08:50 -0600

    Fixed execution errors

--- SkyEditor.SaveEditor/PluginDefinition.vb
+++ SkyEditor.SaveEditor/PluginDefinition.vb
@@ -23,13 +23,13 @@ Public Class PluginDefinition
 
     Public Overrides Sub Load(manager As PluginManager)
         MyBase.Load(manager)
-        manager.RegisterIOFilter("sav", My.Resources.Language.RawSaveFile)
-        manager.RegisterIOFilter("skypkm", My.Resources.Language.SkyPkmFile)
-        manager.RegisterIOFilter("skypkmex", My.Resources.Language.SkyPkmExFile)
-        manager.RegisterIOFilter("skypkmq", My.Resources.Language.SkyPkmQFile)
-        manager.RegisterIOFilter("tdpkm", My.Resources.Language.TDPkmFile)
-        manager.RegisterIOFilter("tdpkmex", My.Resources.Language.TDPkmExFile)
-        manager.RegisterIOFilter("rbpkm", My.Resources.Language.RBPkmFile)
+        manager.RegisterIOFilter("*.sav", My.Resources.Language.RawSaveFile)
+        manager.RegisterIOFilter("*.skypkm", My.Resources.Language.SkyPkmFile)
+        manager.RegisterIOFilter("*.skypkmex", My.Resources.Language.SkyPkmExFile)
+        manager.RegisterIOFilter("*.skypkmq", My.Resources.Language.SkyPkmQFile)
+        manager.RegisterIOFilter("*.tdpkm", My.Resources.Language.TDPkmFile)
+        manager.RegisterIOFilter("*.tdpkmex", My.Resources.Language.TDPkmExFile)
+        manager.RegisterIOFilter("*.rbpkm", My.Resources.Language.RBPkmFile)
         manager.RegisterIOFilter("game_data", My.Resources.Language.GameDataFile)
     End Sub
 End Class
