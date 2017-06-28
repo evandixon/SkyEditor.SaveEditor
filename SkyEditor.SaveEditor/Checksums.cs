57372d83a330d6ae1ababe1af2383011818cf836
Evan Dixon <evanldixon@live.com>
2017-06-28 18:18:02 -0500

    Rewrite checksum calculation

---
+++ SkyEditor.SaveEditor/Checksums.cs
@@ -0,0 +1,33 @@
+﻿using SkyEditor.Core.IO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SkyEditor.SaveEditor
+{
+    /// <summary>
+    /// Utility class to calculate checksums
+    /// </summary>
+    public class Checksums
+    {
+        public static uint Calculate32BitChecksum(BitBlock bits, int startIndex, int endIndex)
+        {
+            ulong sum = 0;
+            for (int i = startIndex; i < endIndex; i += 4)
+            {
+                sum += bits.GetUInt(startIndex, 0, 32) & 0xFFFFFFFF;
+            }
+            return (uint)sum;
+        }
+
+        public static uint Calculate32BitChecksum(GenericFile file, int startIndex, int endIndex)
+        {
+            ulong sum = 0;
+            for (int i = startIndex; i < endIndex; i += 4)
+            {
+                sum += file.ReadUInt32(startIndex) & 0xFFFFFFFF;
+            }
+            return (uint)sum;
+        }
+    }
+}
